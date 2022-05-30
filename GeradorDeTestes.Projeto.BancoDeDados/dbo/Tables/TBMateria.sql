e7e065e6a87174e8d7261e646b53f7964c1d8491
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-05-30 12:23:14 -0300

    Adicionado o projeto de banco de dados no estado atual no Gerador de Testes

---
+++ GeradorDeTestes.Projeto.BancoDeDados/dbo/Tables/TBMateria.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBMateria] (
+    [Numero]            INT           NOT NULL,
+    [Nome]              VARCHAR (150) NOT NULL,
+    [Serie]             INT           NOT NULL,
+    [Disciplina_Numero] INT           NOT NULL,
+    [Disciplina_Nome]   VARCHAR (150) NOT NULL,
+    CONSTRAINT [PK_TBMateria] PRIMARY KEY CLUSTERED ([Numero] ASC),
+    CONSTRAINT [FK_TBMateria_TBMateria] FOREIGN KEY ([Disciplina_Numero]) REFERENCES [dbo].[TBMateria] ([Numero])
+);
+
